dbae49d76df2e51c0d2f066cef08a7c40784c5e2
a <ashell@op.pl>
2019-12-16 22:55:25 +0100

    StaticEx project

---
+++ Szopa/StaticEx/Classes/Tea.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticEx.Classes
+{
+   public static class Tea
+    {
+        public static double Price { get; set; }
+        public static string Name { get; set; }
+    }
+
+    public static class Coffee
+    {
+        public static int Quality { get; set; }
+        public static string Country { get; set; }
+/*
+        StringBuilder builder = new StringBuilder();
+        builder.Append("dgddgf");
+            builder.toString();
+            */
+    }
+    
+    
+    public class Home
+    {
+        public readonly string room;
+
+        public string x (string room)
+        {
+           // room = room;
+        }
+        public static int RoomNumber { get; set; }
+
+        public string Owner{ get; set; }
+
+    }
+
+    public static class Room
+    {
+        public static string Window;
+
+    }
+
+    
+
+}
--- Szopa/StaticEx/Program.cs
+++ Szopa/StaticEx/Program.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using StaticEx;
+//using StaticEx;
+using StaticEx.Classes;
 
 namespace StaticEx
 {
     class Program
     {
-        public static void DoIt()
+        static void Main(string[] args)
         {
-            Console.WriteLine(strength.Coffee());
+            Tea.Name = "Lipton";
+            Tea.Price = 5;
+            Coffee.Country = "Brazylia";
+            Coffee.Quality = 5;
 
+            Console.WriteLine("herbata:" + Tea.Name + ", "+ Tea.Price +"\nkawa: "+  Coffee.Quality + ", "+ Coffee.Country );
+        
             Console.ReadLine();
         }        
     }
