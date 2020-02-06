27ec2f00a1df08a06f00f4ca992fe02022016932
a <ashell@op.pl>
2020-02-06 23:23:45 +0100

    Lesson10 project

---
+++ Szopa/Lesson10/Classes/File.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10.Classes
+{
+    public class File
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+
+        public void Delete()
+        {
+            Console.WriteLine("Delete" + Name);
+        }
+
+    }
+
+    public class GenericFile<T>
+    {
+
+
+        public void Display()
+        {
+            Console.WriteLine("Displaying..");
+        }
+    }
+
+    public class GenericFileList<T> 
+    {
+        public void Add()
+        {
+            Console.WriteLine("adding..");
+        }
+
+    }
+
+
+
+}
+
---
+++ Szopa/Lesson10/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson10.Classes;
+
+namespace Lesson10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //exceptions
+            try
+            {
+                int y = 0;
+                int x = 10 / y;
+                throw new NullReferenceException("ss");
+            }
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            catch (IndexOutOfRangeException exception)
+            {
+                throw;
+            }
+            catch (NullReferenceException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Błąd");
+            }
+
+            //Lists 
+            File file1 = new File();
+            File file2 = new File();
+            File file3 = new File();
+            File file4 = new File();
+
+            List<File> fileList = new List<File>();
+            fileList.Add(file1);
+            fileList.Add(file2);
+            fileList.Add(file3);
+            fileList.Add(file4);
+            fileList.Count();
+            //fileList.Sort();
+
+            foreach (File file in fileList)
+            {
+                file.Delete();
+            }
+
+            //klasy generyczne
+
+            GenericFile<int> iGenericFile = new GenericFile<int>();
+            GenericFile<string> sGenericFile = new GenericFile<string>();
+            iGenericFile.Display();
+            sGenericFile.Display();
+            
+
+            GenericFileList<int> iGenericFileList = new GenericFileList<int>();
+            GenericFileList<string> sGenericFileList = new GenericFileList<string>();
+           
+            sGenericFileList.Add();
+            iGenericFileList.Add();
+
+            Console.ReadLine();
+        }
+    }
+}
